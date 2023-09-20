554b586f0f1e3584f18e4af242dedea2a7f1ab9e
Harsh kasana <bt22ece106@gmail.com>
2023-09-20 14:27:01 +0530

    Added discount bar of hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,17 @@ import { useState } from 'react'
 import {close,logo,menu} from "../assets"
 import {navLinks} from '../constants';
 
-const {toggle,setToggle}=useState(true)
+
 
 const Navbar = () => {
+  const [toggle,setToggle]=useState(false);
   return (
     <nav className='w-full flex py-6 justify-between'>
-      <img src={logo} alt="hoobank" className='w-[124px] h-[32px] ' />
+       <img src={logo} alt="hoobank" className='w-[124px] h-[32px] ' />
 
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+
+
+       <ul className='list-none  sm:flex hidden justify-end items-center flex-1'>
           {navLinks.map((nav,index)=>(
             <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index===navLinks.length-1? 'mr-0': "mr-10"}`}>
               <a href={`#${nav.id}`}>
@@ -19,26 +22,28 @@ const Navbar = () => {
 
             </li>
           ))}
-      </ul>
+        </ul>
 
-      {/* for smaller screen a navbar */}
-      <div className='sm:hidden flex flex-1 justify-end items-center'>
-        <img src={toggle ? close:menu} alt="menu" className='w-[28px] h-[28px] object-contain ' onClick={()=>setToggle((prev)=>!prev)}/>
 
-        <div className={`${toggle? 'flex':'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-        <ul className='list-none flex flex-col  justify-end items-center flex-1'>
-          {navLinks.map((nav,index)=>(
-            <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index===navLinks.length-1? 'mr-0': "mr-10"}`}>
-              <a href={`#${nav.id}`}>
-                {nav.title}
-              </a>
+        <div className='sm:hidden flex felx-1 justify-end items-center'>
+            <img src={toggle?close:menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={()=>setToggle((prev)=>!prev) }/>
 
-            </li>
-          ))}
-      </ul>
+            <div className={`${toggle ? 'flex' : 'hidden'} bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
+                <ul className='list-none flex flex-col  justify-end items-center flex-1'>
+              {navLinks.map((nav,index)=>(
+                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index===navLinks.length-1? 'mr-0': "mb-5"}`}>
+                      <a href={`#${nav.id}`}>
+                        {nav.title}
+                      </a>
+
+                    </li>
+                  ))}
+                </ul>
+            </div>
+          
         </div>
 
-      </div>
+      
     </nav>
   )
 }
